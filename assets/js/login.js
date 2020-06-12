8722123d024c85faff54acb21895a57a26d975ae
xiameng <xiameng.chen@outlook.com>
2020-06-11 20:59:15 -0400

    remove mysql and update js files

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,11 @@
-
-/// Hannah version///
-// $('#loginForm').on('submit',(e) => {
-//     e.preventDefault();
-//     let hash = {};
-//     let arg = ['name','email','password'];
-//     arg.forEach((name) => {
-//         let value = $('#loginForm').find(`[name= ${name}]`).val();
-//         hash[name] = value;
-//     })
-//     $('#loginForm').find('.error').each((index,span) => {
-//         $(span).text('');
-//     })
-//     if(hash['email'] === ''){
-//         $('#loginForm').find('[name = "email"]').siblings('.error').text('Please enter email');
-//         return
-//     }
-//      if(hash['password'] === ''){
-//         $('#loginForm').find('[name = "password"]').siblings('.error').text('Please enter password');
-//         return
-//     }
-//     $.post('/login',hash).then((response) =>{console.log(response)},
-//         (request) => {
-//         let {errors} = request.responseJSON;
-//         if(errors.email  &&  errors.email === 'invalid'){
-//            $('#loginForm').find('[name = "email"]').siblings('.error').text('Invalid Email');
-//         }
-//         })
-//     })
-
 //Ruby fetch
 const loginForm = document.getElementById('loginForm')
+
 loginForm.addEventListener('submit', (evt) => {
      evt.preventDefault()
      let username = evt.target['name'].value
      let password = evt.target['password'].value
+
      fetch("http://localhost:3000/login", {
         method: 'POST',
         headers: {
@@ -49,5 +21,6 @@ loginForm.addEventListener('submit', (evt) => {
     })
     .then(r => r.json())
     .then(console.log)
-}
-)
+
+    // redirect to profile page
+})
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,39 +1,5 @@
-// $('#registerForm').on('submit',(e) => {
-//     e.preventDefault();
-//     let hash = {};
-//     let arg = ['name','email','password'];
-//     arg.forEach((name) => {
-//         let value = $('#registerForm').find(`[name= ${name}]`).val();
-//         hash[name] = value;
-//     })
-//     $('#registerForm').find('.error').each((index,span) => {
-//         $(span).text('');
-//     })
-//     if(hash['name'] === ''){
-//         $('#registerForm').find('[name = "name"]').siblings('.error').text('Please enter full name');
-//         return
-//     }
-//     if(hash['email'] === ''){
-//         $('#registerForm').find('[name = "email"]').siblings('.error').text('Please enter email');
-//         return
-//     }
-//      if(hash['password'] === ''){
-//         $('#registerForm').find('[name = "password"]').siblings('.error').text('Please enter password');
-//         return
-//     }
-//     $.post('/register',hash).then((response) =>{console.log(response)},
-//         (request) => {
-//         let {errors} = request.responseJSON;
-//         if(errors.email  &&  errors.email === 'invalid'){
-//            $('#registerForm').find('[name = "email"]').siblings('.error').text('Invalid Email');
-//         }
-//         })
-//     })
-
-
 /// Ruby fetch
 const registerForm = document.getElementById('registerForm')
-
 
 registerForm.addEventListener('submit', (evt) => {
     evt.preventDefault()
@@ -62,5 +28,4 @@ registerForm.addEventListener('submit', (evt) => {
     })
     .then(r => r.json())
     .then(console.log)
-}
-)
+})
