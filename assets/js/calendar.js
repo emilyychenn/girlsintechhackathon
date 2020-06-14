fed25069590c6c6faeb2d47c77f474489e7d67e2
GAierken <g.aierken@gmail.com>
2020-06-13 23:36:47 -0400

    added dropdown volunteer items and click event

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,10 +1,15 @@
 //find the div
 const calContainerDiv = document.getElementById('calendar-container')
+const volunteerDropdownDiv = document.getElementById("volunteer-dropdown-menu")
 // console.log(calContainerDiv)
+// console.log(volunteerDropdownDiv)
 //create iframe
 
 console.log(localStorage)
 let calIframe = document.createElement("iframe")
+
+
+
 //iframe attribute
 calIframe.style.border = 0
 calIframe.width= "550"
@@ -19,3 +24,35 @@ let iframeSrc = 'https://calendar.google.com/calendar/embed?src='+`${userEmail}`
 calIframe.src=iframeSrc
 /// add iframe to div
 calContainerDiv.append(calIframe)
+
+///get all volunteers
+fetch('http://localhost:3000/users')
+.then(r => r.json())
+.then(data => {
+    let volunteers = data.filter((user) => {
+        return user.is_volunteer
+    }
+    )
+    // console.table(volunteers)
+  
+    // console.log(dropdownItem)
+      /// add dropdown menu
+
+   volunteers.map((volunteer) => {
+        let dropdownItem = document.createElement('a')
+            dropdownItem.className = "dropdown-item" 
+            dropdownItem.href = '#'
+            dropdownItem.innerHTML = volunteer.username
+       
+            volunteerDropdownDiv.append(dropdownItem)
+       ///click appointment fetch post
+            dropdownItem.addEventListener('click', () => {
+                console.log(`${volunteer.username} is clicked`)
+                 //client_id, volunteer_id, appointdate
+            }
+            )
+    }
+    )
+  
+    
+})
